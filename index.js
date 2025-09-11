ef75ec2d6ac5eb1205a3a6818efc2d3c8f34bf26
Rich <richbport@yahoo.com>
2025-09-10 21:37:16 -0400

    function performSearch

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+
+function performSearch () {
+    const query = document.getElementById("searhInput").ariaValueMax;
+    document.getElementById("searchResults").textContent = `${query}`;
+}
