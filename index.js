4d0d20e66cb0979ae83ea1eaed35671952b33fe2
Rich <richbport@yahoo.com>
2025-09-13 15:16:28 -0400

    clear comments

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// function debounce(func, delay) {
-//   let timeoutID;
-//   return function () {
-//     const args = arguments;
-//     const context = this;
-//     clearTimeout(timeoutID);
-//     timeoutID = setTimeout(() => func.apply(context, args), delay)
-//   }
-// }
-
 function debounce(func, delay) {
   let timeoutID;
   return function () {
@@ -18,19 +8,10 @@ function debounce(func, delay) {
   }
 }
 
-// function performSearch() {
-//   const query = document.getElementById("searchInput").value;
-//   document.getElementById("searchResults").textContent = `${query}`;
-// }
-
 function performSearch() {
   const query = document.getElementById("searchInput").value;
   document.getElementById("searchResults").textContent = `${query}`;
 }
-
-// document
-// .getElementById("searchInput")
-// .addEventListener("input", debounce(performSearch, 500));
 
 document
 .getElementById("searchInput")
